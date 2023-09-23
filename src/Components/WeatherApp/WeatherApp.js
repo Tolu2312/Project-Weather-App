7dbb8ac8fb3d730f41e055dd3e4dcb2297b50b91
Tolu2312 <agbooladavid9@gmail.com>
2023-09-23 09:25:54 +0100

    update the weather app

--- src/Components/WeatherApp/WeatherApp.js
+++ src/Components/WeatherApp/WeatherApp.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './WeatherApp.css'
 
 import search_icon from '../Assets/search.png';
@@ -11,12 +11,38 @@ import snow_icon from '../Assets/snow.png';
 import wind_icon from '../Assets/wind.png';
 
 const WeatherApp = () => {
+
+  let api_key ="22a6815b3467add8f28e44268d7cce43";
+
+  
+
+    
+    const search = async () => {
+        const element =document.getElementsByClassName("cityInput")
+        if(element[0].value==="")
+        {
+          return 0;
+        }
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+
+        let response = await fetch(url);
+        let data = await response.json();
+        const humidity =document.getElementsByClassName("humidity-percent")
+        const wind = document.getElementsByClassName("wind-rate")
+        const temperature = document.getElementsByClassName("weather-temp")
+        const location = document.getElementsByClassName("weather-location")
+
+        humidity[0].innerHTML = data.main.humidity+" %";
+        wind[0].innerHTML = data.wind.speed+" km/h";
+        temperature[0].innerHTML = data.main.temp+"c";
+        location[0].innerHTML = data.name;
+    }
   return (
     <div className='container'>
         <div className='top-bar'>
             <input  type='text' className='cityInput' placeholder='Search' />
-            <div className='search-icon' >
-                <img src={search_icon} alt='search-icon' />
+            <div className='search-icon' onClick={() =>{search()}}>
+                <img src={search_icon} alt='' />
 
             </div>
         </div>
@@ -25,10 +51,24 @@ const WeatherApp = () => {
         <img src={cloud_icon} alt='' />
         </div>  
 
-        <div className='weather-temp'>24</div>
+        <div className='weather-temp'>24c</div>
         <div className='weather-location'>Nigeria</div>
         <div className='data-container'>
-          
+          <div className='element'>
+            <img src={humidity_icon} alt='' className='icon' />
+            <div className='data'>
+              <div className='humidity-percent'>64%</div>
+              <div className='text'>Humidity</div>
+            </div>
+          </div>
+
+          <div className='element'>
+            <img src={wind_icon} alt='' className='icon' />
+            <div className='data'>
+              <div className='wind-rate'>18 km/h</div>
+              <div className='text'>Wind Speed</div>
+            </div>
+          </div>
         </div>
     </div>
   );
